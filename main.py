a23533afb9ab93fab8176efd98cf3bd59ea424e2
finito <szww000@126.com>
2019-02-09 20:49:33 +0800

    主函数
    程序入口函数

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   main.py
+@Time    :   2019/02/09 20:47:27
+@Author  :   10110574 
+@Version :   v0.0
+@Contact :   meng.bo1@zte.com.cn
+@License :   (C)Copyright 2018-2019
+@Desc    :   None
+'''
+
+import numpy as np
+
+
